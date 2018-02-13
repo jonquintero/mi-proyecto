1c2e92dcc9ff9f4db3edc4e880de3af5e39e2f3c
jonquintero <jonquintero@hotmail.com>
2018-02-13 17:36:50 -0400

    Leccion 24

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,13 @@ class UserController extends Controller
 
     public function show($id)
     {
-        $user = User::find($id);
+        $user = User::findOrFail($id);
+
+       /* if($user == null)
+        {
+            return response()->view('errors.404',[], 404);
+        }*/
+
         return view('users.show', compact('user'));
 
     }
